654a7c0b3c179c459221e3af5014eb5532bd15b5
Chirantan <mama.godofwar.gamer@gmail.com>
2020-08-12 16:50:22 -0400

    Added a youtube search api call

--- src/react/components/Navheader.js
+++ src/react/components/Navheader.js
@@ -1,30 +1,31 @@
 import React from 'react'
+import YTSearch from 'youtube-api-search'
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap'
 
 export class Navheader extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            def: "Search"
+            videos: []
         }
+        this.term = React.createRef()
+        this.search = this.search.bind(this)
     }
 
-    handleChange(event) {
-        this.setState({
-            def: event.target.value
+    search(term) {
+        YTSearch({key: 'placeholder', term: term}, (videos) => {
+            this.setState({
+                videos: videos
+            })
         })
-    }
-
-    search() {
-
     }
 
     render() {
         return (
             <Navbar className="bg-light justify-content-between">
                 <Form inline>
-                    <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
-                    <Button type="submit" onClick={this.search()}>Submit</Button>
+                    <FormControl ref = {this.term} type="text" placeholder="Search" className=" mr-sm-2" />
+                    <Button type="button" onClick={() => this.search(this.term.current.value)}>Submit</Button>
                 </Form>
             </Navbar>
         )
